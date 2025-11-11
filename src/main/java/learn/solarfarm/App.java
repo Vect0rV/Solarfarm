9e005fe8ec97f35f0c390e5eba43f3d3947811ff
Vect0rV <valmeroth@gmail.com>
2025-11-10 21:42:46 -0600

    Chore: removes unused import statements from App.js

--- src/main/java/learn/solarfarm/App.java
+++ src/main/java/learn/solarfarm/App.java
@@ -1,19 +1,9 @@
 package learn.solarfarm;
 
-import learn.solarfarm.data.PanelFileRepository;
-import learn.solarfarm.domain.PanelService;
-import learn.solarfarm.ui.ConsoleIO;
-import learn.solarfarm.ui.TextIO;
-import learn.solarfarm.ui.View;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-//@PropertySource("classpath:data.properties")
 public class App {
 
     public static void main(String[] args) {
