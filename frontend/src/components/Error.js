83795bbc0c92dbd59f96aa7b38c4d9294c81b7e6
Vect0rV <valmeroth@gmail.com>
2025-12-19 21:39:58 -0600

    chore: adds App.css updates for Error.js message styling

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -1,10 +1,11 @@
 import {  useLocation } from "react-router-dom";
+import "../App.css";
 
 function Error( {i, msg} ) {
     const location = useLocation();
     console.log("msg: ", msg)
 
-    return <p>  {msg}</p>;
+    return <p className="error-msg">* {msg}</p>;
 }
 
 export default Error;
