6ceba909bbd70af55c719ec4f39a0b4bd4b2b5d8
Vect0rV <valmeroth@gmail.com>
2025-12-19 22:51:52 -0600

    chore: updates materialType in PanelRepositoryDouble.java

--- src/test/java/learn/solarfarm/data/PanelRepositoryDouble.java
+++ src/test/java/learn/solarfarm/data/PanelRepositoryDouble.java
@@ -12,7 +12,7 @@ public class PanelRepositoryDouble implements PanelRepository{
     private ArrayList<Panel> panels = new ArrayList<>();
 
     public PanelRepositoryDouble() {
-        panels.add(new Panel(1,"Section 1", 1,5, MaterialType.ASI, Year.of(2020), true));
+        panels.add(new Panel(1,"Section 1", 1,5, MaterialType.A_SI, Year.of(2020), true));
         panels.add(new Panel(2,"Section 2", 4,50, MaterialType.CD_TE,Year.of(2021), true));
         panels.add(new Panel(3,"Section 3", 100,43, MaterialType.MONO_SI,Year.of(2022), true));
     }
