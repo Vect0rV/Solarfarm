63d9c962f2533d0ba56226ab3755951298df15da
Vect0rV <valmeroth@gmail.com>
2025-12-19 21:29:52 -0600

    chore: updates Panel.js to sync with updated backend syntax

--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -2,11 +2,11 @@ import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 
 function Panel({ panel, id, section, row, column, 
-    yearInstalled, material, tracking, onDelete, onEdit }) {
+    installationYear, materialType, isTracking, onDelete, onEdit }) {
 
         function deleteMe() {
             console.log("Delete panel id:", panel.id)
-            onDelete(panel.id);
+            onDelete(panel.panelId);
         }
 
         function editMe() {
@@ -17,11 +17,11 @@ function Panel({ panel, id, section, row, column,
             <div className="p-3 border bg-light">
                 <h3>Section: { section }</h3>
                 <p>Row: { row } Column: { column}</p>
-                <p>Year Installed: {yearInstalled}</p>
-                <p>Material Type: {material}</p>
-                <p>Sun Tracking: {tracking}</p>
+                <p>Year Installed: {installationYear}</p>
+                <p>Material Type: {materialType}</p>
+                <p>Sun Tracking: {isTracking}</p>
                 <div>
-                <Link to={`/edit/${panel.id}`}>Edit Panel</Link>
+                <Link to={`/edit/${panel.panelId}`} className="btn btn-primary">Edit Panel</Link>
                 <Button variant="danger" onClick={deleteMe}>Delete Panel</Button>
                 </div>
             </div>
