a9001a5b04956b9e3e39d008d7501c5e82fa6d66
Vect0rV <valmeroth@gmail.com>
2025-12-19 21:17:26 -0600

    Remove compiled .class files from version control

--- src/main/java/learn/solarfarm/controllers/PanelController.java
+++ src/main/java/learn/solarfarm/controllers/PanelController.java
@@ -1,77 +1,87 @@
-//package learn.solarfarm.controllers;
-//
-//import learn.solarfarm.data.DataAccessException;
-//import learn.solarfarm.domain.*;
-//import learn.solarfarm.domain.PanelService;
-//import learn.solarfarm.models.Panel;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/Panel")
-////@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://127.0.0.1:5500/"})
-//public class PanelController {
-//    private final PanelService service;
-//
-//    public PanelController(PanelService service) {
-//        this.service = service;
-//    }
-//
-//    @GetMapping
-//    public List<Panel> findAll() throws DataAccessException {
-//        return service.findAll();
-//    }
-//
-//    @GetMapping("/section/{section}")
-//    public List<Panel> findBySection(@PathVariable String section) throws DataAccessException {
-//        return service.findBySection(section);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Panel> findById(@PathVariable int id) throws DataAccessException {
-//        Panel Panel = service.findById(id);
-//        if (Panel == null) {
-//            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<>(Panel, HttpStatus.OK);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<?> create(@RequestBody Panel Panel) throws DataAccessException {
-//        PanelResult result = service.add(Panel);
-//        if (!result.isSuccess()) {
-//            return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST); // 400
-//        }
-//        return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED); // 201
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<?> update(@PathVariable int id, @RequestBody Panel Panel) throws DataAccessException {
-//        if (id != Panel.getPanelId()) {
-//            return new ResponseEntity<>(HttpStatus.CONFLICT); // 409
-//        }
-//
-//        Result result = service.update(Panel);
-//        if (!result.isSuccess()) {
-//            if (result.getType() == ResultType.NOT_FOUND) {
-//                return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404
-//            } else {
-//                return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST); // 400
-//            }
-//        }
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> delete(@PathVariable int id) throws DataAccessException {
-//        Result result = service.delete(id);
-//        if (result.getType() == ResultType.NOT_FOUND) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404
-//        }
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
-//    }
-//}
-//
+package learn.solarfarm.controllers;
+
+import learn.solarfarm.data.DataAccessException;
+import learn.solarfarm.domain.PanelWebService;
+import learn.solarfarm.domain.Result;
+import learn.solarfarm.domain.ResultType;
+import learn.solarfarm.models.Panel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/panels")
+public class PanelController {
+
+    private final PanelWebService service;
+
+    public PanelController(PanelWebService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Panel> findAll() throws DataAccessException {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Panel> findById(@PathVariable int id) throws DataAccessException {
+        Panel panel = service.findById(id);
+        if (panel == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(panel, HttpStatus.OK);
+    }
+
+    @GetMapping("/section/{section}")
+    public ResponseEntity<List<Panel>> findBySection(@PathVariable String section) throws DataAccessException {
+        List<Panel> panels = service.findBySection(section);
+        if (panels == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(panels, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Panel panel) throws DataAccessException {
+        Result<Panel> result = service.add(panel);
+
+        if (!result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+
+        if (result.getType() == ResultType.INVALID) {
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{panelId}")
+    public ResponseEntity<Object> update(@PathVariable("panelId") int panelId,@RequestBody Panel panel) throws DataAccessException {
+        panel.setPanelId(panelId);
+
+        if (panelId != panel.getPanelId()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Result<Panel> result = service.update(panel);
+        if (result.getType() == ResultType.INVALID) {
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        } else if (result.getType() == ResultType.NOT_FOUND) {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result.getMessages(), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{panelId}")
+    public ResponseEntity<Void> delete(@PathVariable("panelId") int panelId) throws DataAccessException {
+        if(service.delete(panelId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+}
